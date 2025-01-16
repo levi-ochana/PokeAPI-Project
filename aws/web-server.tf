17abd7a7d51cf4e1cdd8dde28ef50d907c528c76
levi ochana <ochana7702@gmail.com>
2025-01-17 00:23:58 +0200

    Update web-server.tf

--- aws/web-server.tf
+++ aws/web-server.tf
@@ -1,7 +1,7 @@
 # Create an EC2 Instance for web-server Game 
 resource "aws_instance" "web-server" {
   ami           = "ami-055e3d4f0bbeb5878"  # Amazon Linux 2 AMI ID
-  instance_type = "t2.micro"  # Free tier instance type
+  instance_type = var.instance_type  # Free tier instance type
   key_name = data.aws_key_pair.poke_key_pair.key_name
   subnet_id     = aws_subnet.poke_subnet.id
   security_groups = [aws_security_group.poke_sg.id]
